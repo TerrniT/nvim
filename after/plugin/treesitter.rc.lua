20a355b40532fb5b565bb84e1faac48b4c32dcc9
terrnit <terrnit.megaman@gmail.com>
2022-08-11 12:32:13 +0500

    fix: treesitter fixed - highlighting parts of code working well

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -1,22 +1,17 @@
-local status, ts = pcall(require, 'nvim-tresitter.configs')
-if (not status) then return end
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
 
-ts.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    'tsx',
-    'lua',
-    'json',
-    'css'
-  },
-  autotag = {
-    enable = true,
-  }
-}
+configs.setup({
+	ensure_installed = "all", -- one of "all" or a list of languages
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+	},
+	autopairs = {
+		enable = true,
+	},
+	indent = { enable = true, disable = { "python", "css" } },
+})
